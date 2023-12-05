b26656b3b640436cf0f4a1df9b5a0c714c7b85af
Mikael Tranbom <mikael@tranbom.io>
2023-12-05 19:24:37 +0100

    Update docstrings

--- yt_dlp_ctk/__init__.py
+++ yt_dlp_ctk/__init__.py
@@ -1,4 +1,18 @@
 """
-Init
+yt-dlp-ctk - GUI for yt-dlp
+
+Copyright (C) 2023 Mikael Tranbom
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 from yt_dlp_ctk import _version  # noqa: F401
